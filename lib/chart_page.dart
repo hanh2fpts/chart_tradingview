3bb3256117f6413dc444cb415be67ac7ac3bc1b8
hanh2fpts <hanh2@fpts.com.vn>
2023-07-12 17:27:35 +0700

    add library

--- lib/chart_page.dart
+++ lib/chart_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 
 class ChartPage extends StatefulWidget {
   const ChartPage({super.key});
@@ -11,8 +12,35 @@ class _ChartPageState extends State<ChartPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Container(),
-    );
+        appBar: AppBar(),
+        body: Center(
+          child: SizedBox(
+              height: 500,
+              child: InAppWebView(
+                initialUrlRequest: URLRequest(
+                    url: Uri.parse(
+                        "http://localhost:8888/assets/charting_library/mobile_black.html")),
+                androidOnPermissionRequest: (controller, origin, resources) async {
+                  return PermissionRequestResponse(
+                      resources: resources, action: PermissionRequestResponseAction.GRANT);
+                },
+                onWebViewCreated: (controller) {
+                  //String script = "setEnvironment(\"VNINDEX\", \"VI\");";
+                  //controller.evaluateJavascript(source: script);
+                },
+                onLoadStart: (controller, url) {
+                  print('loadStart $url');
+                },
+                onLoadStop: (controller, url) {
+                  String script = "setEnvironment(\"VNINDEX\", \"VI\");";
+                  controller.evaluateJavascript(source: script);
+                  //_controller = controller;
+                  print('loadStop $url');
+                },
+                onConsoleMessage: (controller, message) {
+                  print('logWeb ${message.message}');
+                },
+              )),
+        ));
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,36 @@
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 
 import 'chart_page.dart';
 
-void main() => runApp(const MyApp());
+final InAppLocalhostServer localhostServer = InAppLocalhostServer(port: 8888);
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  localhostServer.start();
+  if (Platform.isAndroid) {
+    await AndroidInAppWebViewController.setWebContentsDebuggingEnabled(true);
+    var swAvailable = await AndroidWebViewFeature.isFeatureSupported(
+        AndroidWebViewFeature.SERVICE_WORKER_BASIC_USAGE);
+    var swInterceptAvailable = await AndroidWebViewFeature.isFeatureSupported(
+        AndroidWebViewFeature.SERVICE_WORKER_SHOULD_INTERCEPT_REQUEST);
+    if (swAvailable && swInterceptAvailable) {
+      AndroidServiceWorkerController serviceWorkerController =
+          AndroidServiceWorkerController.instance();
+      await serviceWorkerController.setServiceWorkerClient(AndroidServiceWorkerClient(
+        shouldInterceptRequest: (request) async {
+          if (kDebugMode) {
+            print(request);
+          }
+          return null;
+        },
+      ));
+    }
+    runApp(const MyApp());
+  }
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
